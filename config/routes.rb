964aa2295125036ca7b2f8273ddcc5a6ac262cc6
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-10-04 00:53:39 +0200

    Authorize spaces again in paths.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'audiobooks#show'
 
-  get 'audiobooks/*path', to: 'audiobooks#show', as: :content, constraints: { path: /[\w+\.]+/ }
-  get 'stream/*path', to: 'audiobooks#stream', as: :stream, constraints: { path: /[\w+\.]+/ }
+  get 'audiobooks/*path', to: 'audiobooks#show', as: :content, constraints: { path: /.+/ }
+  get 'stream/*path', to: 'audiobooks#stream', as: :stream, constraints: { path: /.+/ }
 end
